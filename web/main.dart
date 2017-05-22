d328475ed0e3d26d83e73d28d6cdfc3c90babd5b
Dmitriy.Litovchenko <Dmitriy.Litovchenko@yamalspg.ru>
2017-05-22 13:43:33 +0300

    ng 3.1.0 maintenance

--- web/main.dart
+++ web/main.dart
@@ -1,12 +1,17 @@
 import 'dart:core';
-import 'dart:html';
+import 'dart:html' as h;
 
 import 'package:angular2/platform/browser.dart';
 import 'package:angular2/core.dart';
-import 'package:angular2/src/core/reflection/reflection.dart';
 import 'package:angular2/router.dart';
 import 'package:angular2/platform/common.dart';
 
+import 'package:http/http.dart';
+import 'package:http/browser_client.dart';
+
+import 'package:aside/aside_service.dart';
+import 'package:auth/auth_service.dart';
+import 'package:config/config_service.dart';
 import 'package:master_layout/master_layout_component.dart';
 
 import 'package:alert/alert_component.dart';
@@ -16,40 +21,41 @@ bool get isDebug =>
     (const String.fromEnvironment('PRODUCTION', defaultValue: 'false')) !=
     'true';
 
-@Component(selector: 'app')
-@View(
-  template: '<master-layout><alert></alert></master-layout>',
-  directives: const [MasterLayoutComponent, AlertComponent])
+@Component(
+    selector: 'app',
+    template: '<master-layout><alert></alert></master-layout>',
+    directives: const [MasterLayoutComponent, AlertComponent])
 class AppComponent {}
 
 main() async {
-  if (isDebug) {
-    reflector.trackUsage();
-  }
-
   ComponentRef ref = await bootstrap(AppComponent, [
     ROUTER_PROVIDERS,
     const Provider(LocationStrategy, useClass: HashLocationStrategy),
     const Provider(AlertService),
-    const Provider(MasterLayoutComponent)]);
+    const Provider(AsideService),
+    const Provider(ConfigService),
+    const Provider(AuthenticationService),
+    const Provider(AuthorizationService),
+    const Provider(MasterLayoutComponent),
+    provide(Client, useFactory: () => new BrowserClient(), deps: [])
+  ]);
 
   var alertService = ref.injector.get(AlertService);
 
-  querySelector('#show-danger')
+  h.querySelector('#show-danger')
     ..onClick.listen((_) => alertService.Danger('danger text'));
 
-  querySelector('#show-success')
+  h.querySelector('#show-success')
     ..onClick.listen((_) => alertService.Success('success text'));
 
-  querySelector('#show-info')
+  h.querySelector('#show-info')
     ..onClick.listen((_) => alertService.Info('info text'));
 
-  querySelector('#show-warning')
+  h.querySelector('#show-warning')
     ..onClick.listen((_) => alertService.Warning('warning text'));
 
   if (isDebug) {
     print('Application in DebugMode');
     enableDebugTools(ref);
-    print('Unused keys: ${reflector.listUnusedKeys()}');
   }
 }
